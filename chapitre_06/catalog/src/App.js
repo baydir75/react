4e8b4f700ab7849cbe7b634e86b7cbb540554a52
baydir <kidoni@live.fr>
2021-08-08 14:50:00 +0200

    final

--- chapitre_06/catalog/src/App.js
+++ chapitre_06/catalog/src/App.js
@@ -1,22 +1,28 @@
 import React from 'react'
-
+import { BrowserRouter, Route, Link, Switch } from "react-router-dom"
 import Home from './Components/Home'
+import FilmInfo from './Components/FilmInfo'
 
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-  
+
     this.state = {
-       
+
     }
   }
 
   render() {
     return (
-      <div>
-        <Home/>
-      </div>
+      <BrowserRouter>
+        <div>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/film/:id" component={FilmInfo} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     )
   }
 
--- chapitre_06/catalog/src/Components/FilmInfo.js
+++ chapitre_06/catalog/src/Components/FilmInfo.js
@@ -1,19 +1,32 @@
 import React from 'react'
-import Film from '../data/films.json'
+import Films from '../data/films.json'
 
 class FilmInfo extends React.Component {
     constructor(props) {
         super(props)
-    
+
         this.state = {
-             
+
         }
     }
-    
+
     render() {
+        console.log(Films[0])
+        const filmTest = Films.filter(film => film.id === parseInt(this.props.match.params.id))
+        console.log(filmTest)
         return (
             <div>
-                <p>Test</p>
+                <h1>{filmTest[0].title}</h1>
+                <img src={filmTest[0].image} alt="poster"></img>
+                <p>{filmTest[0].director}</p>
+                <ul>
+                    {filmTest[0].stars.map(stars => {
+                        return (
+                            <li>{stars}</li>
+                        )
+                    })}
+                </ul>
+                <p>{filmTest[0].description}</p>
             </div>
         )
     }
